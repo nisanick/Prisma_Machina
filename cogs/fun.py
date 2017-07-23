31844f635460896d83d513b4864c1771f5811701
nisanick <nisanick@gmail.com>
2017-07-23 23:08:17 +0200

    funzies

--- cogs/fun.py
+++ cogs/fun.py
@@ -25,7 +25,6 @@ class Fun:
             await ctx.send(random.choice(options))
         await ctx.message.delete("Command cleanup")
 
-    @commands.Bot.event()
     async def on_message(self, message: discord.Message):
         print(message.content)
         if message.author.id == 186829544764866560 and message.content.lower().__contains__("by achenar"):
