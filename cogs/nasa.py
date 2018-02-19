2bed129e54bfe15b181df0ba2efb7ad884eb4abb
Robert Hudec <hudec@offeris.com>
2018-02-19 15:11:06 +0100

    fix

--- cogs/nasa.py
+++ cogs/nasa.py
@@ -14,13 +14,20 @@ class Nasa:
         """
         Shows Astronomical Picture of the Day straight from NASA
         """
+        boundary = 1000;
         response = await Web.get_response("https://api.nasa.gov/planetary/apod?api_key={}".format(config.NASA_API))
         embed = discord.Embed(title='Astronomy Picture of the Day', description='**{}** | {}'.format(response["title"], response["date"]))
-        embed.add_field(name='Explanation', value=response['explanation'][0: 1000], inline=False)
+        embed.add_field(name='Explanation', value=response['explanation'][0: boundary], inline=False)
+
+        while boundary < len(response['explanation']):
+            embed.add_field(value=response['explanation'][boundary: boundary + 1000])
+            boundary = boundary + 1000
+
         try:
             embed.add_field(name='HD Download', value='[Click here!]({})'.format(response["hdurl"]))
         except Exception:
             print('No HD ulr')
+        embed.add_field(name='url', value='[url]({})'.format(response['url']))
         embed.set_image(url=response['url'])
         embed.timestamp = datetime.utcnow()
         embed.set_footer(text='Generated on ')
