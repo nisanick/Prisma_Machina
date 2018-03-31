db4df6dc66c42f18febf0f8d3e0b821d41f76667
nisanick <nisanick@gmail.com>
2018-03-31 16:57:42 +0200

    timers

--- cogs/fun.py
+++ cogs/fun.py
@@ -50,11 +50,17 @@ class Fun:
             await ctx.send(
                 "Her awesomeness will be greatly missed in our hearts. May her soul find its place amongst the eagles of the Empire.")
         else:
-            await ctx.send("You will never be as awesome as Wisewolf")
+            if datetime.strptime('01.04.{}'.format(datetime.utcnow().year), '%d.%m.%Y') == datetime.utcnow():
+                await ctx.send("Everyone is better than Wisewolf")
+            else:
+                await ctx.send("You will never be as awesome as Wisewolf")
 
     @commands.command(hidden=True)
     async def report(self, ctx, *, message=None):
-        await ctx.send("This incident was reported to proper authorities!")
+        if datetime.strptime('01.04.{}'.format(datetime.utcnow().year), '%d.%m.%Y') == datetime.utcnow():
+            await ctx.send("{} was reported to proper authorities!".format(ctx.author.nick or ctx.author.name))
+        else:
+            await ctx.send("This incident was reported to proper authorities!")
         if not isinstance(ctx.channel, discord.DMChannel):
             await ctx.message.delete()
 
@@ -137,6 +143,8 @@ class Fun:
             if datetime.strptime('24.12.{}'.format(datetime.utcnow().year), '%d.%m.%Y') < datetime.utcnow() < datetime.strptime(
                     '27.12.{}'.format(datetime.utcnow().year), '%d.%m.%Y'):
                 reaction = '🎁'
+            elif datetime.strptime('01.04.{}'.format(datetime.utcnow().year), '%d.%m.%Y') == datetime.utcnow():
+                reaction = '🔫'
 
             await asyncio.sleep(random.randint(1, 60))
             await self.duck_message.add_reaction(reaction)
@@ -152,6 +160,8 @@ class Fun:
         if datetime.strptime('24.12.{}'.format(datetime.utcnow().year), '%d.%m.%Y') < datetime.utcnow() < datetime.strptime(
                 '27.12.{}'.format(datetime.utcnow().year), '%d.%m.%Y'):
             hunt = '🎁'
+        elif datetime.strptime('01.04.{}'.format(datetime.utcnow().year), '%d.%m.%Y') == datetime.utcnow():
+            hunt = '🦆'
         if reaction.emoji == hunt and reaction.message == self.duck_message:
             self.users.append(user.id)
             self.duck_shots += 1
